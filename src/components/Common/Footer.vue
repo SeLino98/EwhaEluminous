6735be0c27fdca291ec171f8d0251990fcce35cc
unknown <tbc07061@naver.com>
2023-11-09 23:31:18 +0900

    header + footer + home

---
+++ src/components/Common/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <b-icon class="h4 mb-2" icon="instagram" onclick = "location.href = 'https://www.instagram.com/e_luminous_2019/'"/>
+</template>
+
+<script>
+export default {
+    name: 'FooterView',
+}
