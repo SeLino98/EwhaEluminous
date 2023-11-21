4331501ac2ca04e3c054a8778957d78a656f73bc
CNH <zetta_byte@naver.com>
2023-11-21 21:46:15 +0900

    config.js 수정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // build 경로 설정
+  outputDir: "../../IdeaProjects/EwhaEluminous-BE/EwhaEluminous-BE/backend/src/main/resources/static",
+  devServer: {
+    // 프록시 설정
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
+
 })
